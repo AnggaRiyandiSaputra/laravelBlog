d45fe5a266f66ee5ebf42e9c2fa7a24f4a0f87ad
AnggaRiyandiSaputra <anggariyandisaputra1@gmail.com>
2025-03-28 03:24:55 +0700

    add slug generate for posts; ensure unique slugs on update

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Posts extends Model
 {
@@ -28,11 +29,38 @@ class Posts extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 
+    public static function generateSlug($title, $id = null)
+    {
+        $slug = Str::slug($title);
+        $originalSlug = $slug;
+        $count = 1;
+
+        while (self::where('slug', $slug)
+            ->when($id, function ($query) use ($id) {
+                return $query->where('id', '!=', $id);
+            })->exists()
+        ) {
+            $slug = $originalSlug . '-' . $count;
+            $count++;
+        }
+
+        return $slug;
+    }
+
     public static function getPostByUserLogin()
     {
         $user = Auth::user();
         return self::where('user_id', $user->id)
             ->orderBy('id', 'desc')
             ->paginate(10);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::updating(function ($model) {
+            $model->slug = self::generateSlug($model->title, $model->id);
+        });
     }
 }
