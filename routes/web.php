a6ddf8791687447ca18cdb80d41d349a1f9a5f4c
AnggaRiyandiSaputra <anggariyandisaputra1@gmail.com>
2025-03-27 19:29:23 +0700

    add BlogController and routes for member blogs

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Member\BlogController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //member
+    Route::resource('member/blogs', BlogController::class)->names([
+        'index' => 'member.blogs.index',
+        'create' => 'member.blogs.create',
+        'store' => 'member.blogs.store',
+        'show' => 'member.blogs.show',
+        'edit' => 'member.blogs.edit',
+        'update' => 'member.blogs.update',
+        'destroy' => 'member.blogs.destroy',
+    ]);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
